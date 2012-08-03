fbeb2b30afe1aa981a3df2abea549bd38e0a89df
Steven Haryanto <stevenharyanto@gmail.com>
2012-08-03 08:03:36 +0000

    Import of SHARYANTO/Task-BeLike-SHARYANTO-Patches-0.01 from CPAN.
    gitpan-cpan-distribution: Task-BeLike-SHARYANTO-Patches
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SHARYANTO/Task-BeLike-SHARYANTO-Patches-0.01.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/Task/BeLike/SHARYANTO/Patches.pm
@@ -0,0 +1,48 @@
+package Task::BeLike::SHARYANTO::Patches;
+
+our $VERSION = '0.01'; # VERSION
+
+
+1;
+# ABSTRACT: SHARYANTO's patch modules
+
+__END__
+=pod
+
+=head1 NAME
+
+Task::BeLike::SHARYANTO::Patches - SHARYANTO's patch modules
+
+=head1 VERSION
+
+version 0.01
+
+=head1 DESCRIPTION
+
+This task will install all SHARYANTO's patch modules.
+
+=head1 TASK CONTENTS
+
+=head2 Included modules
+
+=head3 L<HTTP::Daemon::patch::ipv6>
+
+=head3 L<LWP::UserAgent::patch::https_hard_timeout>
+
+=head3 L<LWP::UserAgent::patch::log_response>
+
+=head3 L<Net::HTTP::Methods::patch::log_request>
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
